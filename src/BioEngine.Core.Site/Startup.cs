9bcd381bd7a37acc899090ca01204580ac510ab9
George <sonicgd@gmail.com>
2019-05-15 12:48:48 +0500

    Rearrange app startups.

--- src/BioEngine.Core.Site/Startup.cs
+++ src/BioEngine.Core.Site/Startup.cs
@@ -1,79 +1,21 @@
-using System.Globalization;
+using BioEngine.Core.Web;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.Core.Site
 {
-    public abstract class BioEngineStartup
+    public abstract class BioEngineSiteStartup : BioEngineWebStartup
     {
-        protected readonly IConfiguration Configuration;
-
-        protected BioEngineStartup(IConfiguration configuration)
+        protected BioEngineSiteStartup(IConfiguration configuration) : base(configuration)
         {
-            Configuration = configuration;
         }
 
-        public virtual void ConfigureServices(IServiceCollection services)
+        protected override void ConfigureBeforeRouting(IApplicationBuilder app, IHostEnvironment env)
         {
-            services.AddControllersWithViews()
-                .AddNewtonsoftJson()
-                .SetCompatibilityVersion(CompatibilityVersion.Latest);
-
-            services.AddHealthChecks();
-        }
-
-        public virtual void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            var supportedCultures = new[] {new CultureInfo("ru-RU"), new CultureInfo("ru")};
-
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("ru-RU"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            });
-
-            app.UseStaticFiles();
-
-            if (env.IsProduction())
-            {
-                var options = new ForwardedHeadersOptions
-                {
-                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-                };
-                options.KnownProxies.Clear();
-                options.KnownNetworks.Clear();
-                options.RequireHeaderSymmetry = false;
-                app.UseForwardedHeaders(options);
-            }
-            
             app.UseStatusCodePagesWithReExecute("/error/{0}");
 
             app.UseMiddleware<CurrentSiteMiddleware>();
-            
-            app.UseRouting();
-
-            ConfigureApp(app, env);
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health");
-            });
-        }
-
-        protected virtual void ConfigureApp(IApplicationBuilder app, IHostEnvironment env)
-        {
         }
     }
 }
